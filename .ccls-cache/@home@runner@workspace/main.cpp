9eb6fa51d703edaaeacef719de6b0477d246a4ac
christiannastropoli <cys80729@email.vccs.edu>
2025-12-14 21:46:44 +0000

    Modified code.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
-
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+int cookies;
+std::cout << "How many cookies do you want to make? ";
+std::cin >> cookies;
+double sugar = 1.5 * (cookies / 48.0);
+double butter = 1.0 * (cookies / 48.0);
+double flour = 2.75 * (cookies / 48.0);
+std::cout << "Cups of sugar needed: " << sugar << std::endl;
+std::cout << "Cups of butter needed: " << butter << std::endl;
+std::cout << "Cups of flour needed: " << flour << std::endl;
+return 0;
+}
